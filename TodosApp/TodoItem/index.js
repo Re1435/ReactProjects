4334afcd5f9bedccfaa6e7e6a38370fe49186df0
Re1435 <reddymahereddy143@gmail.com>
2023-05-18 14:00:23 +0530

    Commit the todoApp code into repo

---
+++ TodosApp/TodoItem/index.js
@@ -0,0 +1,23 @@
+// Write your code here
+// Write your code here
+import './index.css'
+
+const TodoItem = props => {
+  const {todoDetails, deleteTodo} = props
+  const {id, title} = todoDetails
+
+  const onDelete = () => {
+    deleteTodo(id)
+  }
+
+  return (
+    <li className="todos-list">
+      <p className="title">{title}</p>
+      <button className="button" type="button" onClick={onDelete}>
+        Delete
+      </button>
+    </li>
+  )
+}
+
+export default TodoItem
