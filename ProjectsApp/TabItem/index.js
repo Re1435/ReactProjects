9d913e8aec6ade12715aebb8d3fbebcdc6b0ac72
Re1435 <reddymahereddy143@gmail.com>
2023-05-24 17:41:40 +0530

    Commit the ProjectsApp Code into repo

---
+++ ProjectsApp/TabItem/index.js
@@ -0,0 +1,23 @@
+import './index.css'
+
+const TabItem = props => {
+  const {tabDetails, updateActiveTabId, isActive} = props
+  const {tabId, displayText} = tabDetails
+  const onClickActiveTabId = () => {
+    updateActiveTabId(tabId)
+  }
+  const activeTabClassName = isActive ? 'active-tab-btn' : ''
+  return (
+    <li className="tab-item-container ">
+      <button
+        type="button"
+        className={`tab-btn ${activeTabClassName}`}
+        onClick={onClickActiveTabId}
+      >
+        {displayText}
+      </button>
+    </li>
+  )
+}
+
+export default TabItem
